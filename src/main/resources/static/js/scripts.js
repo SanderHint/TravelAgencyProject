4cedfba7f21a6254e428f2ad9d2fb73d103347b0
SanderHint <sander.hintsov@gmail.com>
2023-08-14 21:15:42 +0300

    Additional changes  made to the search bar

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -22,6 +13,13 @@ window.addEventListener('DOMContentLoaded', event => {
         }
 
     };
+
+    // Search bar movement
+    $(document).ready(function() {
+        $('#searchToggle').click(function() {
+            $('#searchBar').toggleClass('active');
+        });
+    });
 
     // Shrink the navbar 
     navbarShrink();
@@ -51,4 +49,62 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+});
+
+const express = require('express');
+const app = express();
+
+// ... other middleware and configurations ...
+
+// Define a route to handle the search
+app.get('/search', (req, res) => {
+    const query = req.query.q; // Get the search query from the 'q' parameter
+
+    // Implement your search logic here
+    // For demonstration, let's assume searchResults is an array of search results
+    const searchResults = [
+        'Result 1',
+        'Result 2',
+        'Result 3',
+        // ... more search results ...
+    ];
+
+    res.json({ results: searchResults });
+});
+
+window.addEventListener('DOMContentLoaded', event => {
+    // ... existing code ...
+
+    // Handle search form submission
+    const searchForm = document.querySelector('#searchForm');
+    const searchResultsContainer = document.querySelector('#searchResults');
+
+    searchForm.addEventListener('submit', async event => {
+        event.preventDefault();
+
+        const formData = new FormData(searchForm);
+        const searchQuery = formData.get('q');
+
+        if (searchQuery) {
+            try {
+                const response = await fetch(`/search?q=${searchQuery}`);
+                const data = await response.json();
+                const searchResults = data.results;
+
+                // Clear previous search results
+                searchResultsContainer.innerHTML = '';
+
+                // Display search results
+                searchResults.forEach(result => {
+                    const resultItem = document.createElement('div');
+                    resultItem.textContent = result;
+                    searchResultsContainer.appendChild(resultItem);
+                });
+            } catch (error) {
+                console.error('Error fetching search results:', error);
+            }
+        }
+    });
+
+    // ... rest of the code ...
 });
